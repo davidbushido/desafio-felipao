27897bace64c3fe6126a0a549b9cb11536632d06
davidbushido <davidbushido21@gmail.com>
2023-12-01 16:11:27 -0300

    completei o desafio

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+let nomeDoHeroi = "Bushido"
+let xpDoHeroi = 7500    
+
+
+let nivel;
+if (xpDoHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log ("O herói de nome " + nomeDoHeroi + " está no nível " + nivel);
+
+console.log ("Obrigado pelos ensinamentos, Felipão :) ")
